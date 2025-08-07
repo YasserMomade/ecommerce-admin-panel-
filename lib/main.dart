9bcc2431117e5bbb2dfbb8c6449e8988abe4730f
Yasser Momade <yassermomade11@gmail.com>
2025-08-07 07:21:50 +0200

    past3

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
-import 'package:admin/utility/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'core/data/data_provider.dart';
 import 'core/routes/app_pages.dart';
+import 'screens/brands/provider/brand_provider.dart';
 import 'screens/category/provider/category_provider.dart';
+import 'screens/coupon_code/provider/coupon_code_provider.dart';
+import 'screens/dashboard/provider/dash_board_provider.dart';
 import 'screens/main/main_screen.dart';
 import 'screens/main/provider/main_screen_provider.dart';
+import 'screens/notification/provider/notification_provider.dart';
+import 'screens/order/provider/order_provider.dart';
+import 'screens/posters/provider/poster_provider.dart';
+import 'screens/sub_category/provider/sub_category_provider.dart';
+import 'screens/variants/provider/variant_provider.dart';
+import 'screens/variants_type/provider/variant_type_provider.dart';
 import 'utility/constants.dart';
-import '../../../utility/extensions.dart';
+import 'utility/extensions.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +25,15 @@ void main() {
     ChangeNotifierProvider(create: (context) => DataProvider()),
     ChangeNotifierProvider(create: (context) => MainScreenProvider()),
     ChangeNotifierProvider(create: (context) => CategoryProvider(context.dataProvider)),
-
+    ChangeNotifierProvider(create: (context) => SubCategoryProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => BrandProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => VariantsTypeProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => VariantsProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => DashBoardProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => CouponCodeProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => PosterProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => OrderProvider(context.dataProvider)),
+    ChangeNotifierProvider(create: (context) => NotificationProvider(context.dataProvider)),
   ], child: MyApp()));
 }
 
--- lib/screens/main/provider/main_screen_provider.dart
+++ lib/screens/main/provider/main_screen_provider.dart
@@ -1,17 +1,17 @@
-//import '../../brands/brand_screen.dart';
+import '../../brands/brand_screen.dart';
 import '../../category/category_screen.dart';
-//import '../../coupon_code/coupon_code_screen.dart';
-//import '../../dashboard/dashboard_screen.dart';
-//import '../../notification/notification_screen.dart';
-//import '../../order/order_screen.dart';
-//import '../../posters/poster_screen.dart';
-//import '../../variants/variants_screen.dart';
-//import '../../variants_type/variants_type_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../coupon_code/coupon_code_screen.dart';
+import '../../dashboard/dashboard_screen.dart';
+import '../../notification/notification_screen.dart';
+import '../../order/order_screen.dart';
+import '../../posters/poster_screen.dart';
+import '../../sub_category/sub_category_screen.dart';
+import '../../variants/variants_screen.dart';
+import '../../variants_type/variants_type_screen.dart';
 
-//import '../../sub_category/sub_category_screen.dart';
 
 class MainScreenProvider extends ChangeNotifier{
   Widget selectedScreen = DashboardScreen();
